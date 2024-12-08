3c4db26b7860ccd0f3c8188f59f292f3f9340df8
christian mabeladi <christianmabeladi@gmail.com>
2024-12-08 08:14:41 +0100

    delete dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,16 +1,3 @@
-import DashboardClient from "./dashboard-client";
-import { fetchAdmins, fetchRegistrations } from "./feth"; // Correction ici
-
 export default async function Dashboard() {
-  try {
-    const registrations = await fetchRegistrations();
-    const admins = await fetchAdmins();
-
-    return (
-      <DashboardClient initialRegistrations={registrations} admins={admins} />
-    );
-  } catch (error) {
-    console.error("Erreur lors de la récupération des données :", error);
-    return <div>Une erreur est survenue lors du chargement des données.</div>;
-  }
+  return <div>Dashboard</div>;
 }
